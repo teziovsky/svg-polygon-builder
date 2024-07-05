dc4bde6797c4077c951c1f2697d0a0ba4e6a646f
Jakub Soboczyński <teziovsky@gmail.com>
2024-07-05 09:28:38 +0200

    fix: zoom respect when add point

--- src/App.vue
+++ src/App.vue
@@ -58,14 +58,16 @@ function deletePolygon(polygonId: number) {
   }
 }
 
+const zoom = ref(1);
+
 function addPoint(event: MouseEvent) {
   if (!polygons.value.length) {
     addPolygon();
   }
 
   const canvas = event.target as HTMLDivElement;
-  const canvasMaxX = canvas.clientWidth;
-  const canvasMaxY = canvas.clientHeight;
+  const canvasMaxX = canvas.clientWidth * zoom.value;
+  const canvasMaxY = canvas.clientHeight * zoom.value;
 
   const cursorX = event.clientX;
   const cursorY = event.clientY;
@@ -74,11 +76,11 @@ function addPoint(event: MouseEvent) {
   const positionY = positionRect.top;
   const positionX = positionRect.left;
 
-  const relatedY = cursorY - positionY;
-  const relatedX = cursorX - positionX;
+  const adjustedCursorX = (cursorX - positionX) / zoom.value;
+  const adjustedCursorY = (cursorY - positionY) / zoom.value;
 
-  const resultY = Math.round((relatedY / canvasMaxY) * 1000) / 10;
-  const resultX = Math.round((relatedX / canvasMaxX) * 1000) / 10;
+  const resultY = Math.round((adjustedCursorY / canvasMaxY) * 1000 * zoom.value) / 10;
+  const resultX = Math.round((adjustedCursorX / canvasMaxX) * 1000 * zoom.value) / 10;
 
   const polygon = polygons.value.find((polygon) => polygon.id === activeElementId.value);
 
@@ -119,35 +121,20 @@ function handleChangeFile(event: Event) {
   }
 }
 
-const imageWrapper = ref<HTMLDivElement | null>(null);
+const step = ref(0.1);
+const steps = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
 
 function zoomIn() {
-  if (imageWrapper.value) {
-    const actualScale = imageWrapper.value.style.scale;
-
-    if (actualScale) {
-      const scale = parseFloat(actualScale) + 0.1;
-      imageWrapper.value.style.scale = `${scale}`;
-    } else {
-      imageWrapper.value.style.scale = "1.1";
-    }
-  }
+  zoom.value = zoom.value + step.value;
 }
 
 function zoomOut() {
-  if (imageWrapper.value) {
-    const actualScale = imageWrapper.value.style.scale;
+  const newZoom = zoom.value - step.value;
 
-    if (actualScale) {
-      const scale = parseFloat(actualScale) - 0.1;
-      console.log("(App.vue:145) scale: ", scale);
-
-      if (scale < 1) return;
-
-      imageWrapper.value.style.scale = `${scale}`;
-    } else {
-      imageWrapper.value.style.scale = "1";
-    }
+  if (newZoom < 1) {
+    zoom.value = 1;
+  } else {
+    zoom.value = newZoom;
   }
 }
 
@@ -178,7 +165,7 @@ onBeforeUnmount(() => {
       @click="addPoint"
       @contextmenu="(e) => e.preventDefault()"
     >
-      <div ref="imageWrapper" class="origin-top-left transition-[scale]">
+      <div :style="{ scale: zoom }" class="origin-top-left transition-[scale]">
         <img :height="imageHeight" :src="imageSrc" :width="imageWidth" alt="" class="h-auto w-full object-cover" />
         <div class="absolute inset-0 pr-[5%]" @contextmenu="removePoint">
           <svg class="block h-full w-full cursor-copy" preserveAspectRatio="none" viewBox="0 0 100 100">
@@ -201,22 +188,31 @@ onBeforeUnmount(() => {
           <label class="sr-only inline-block" for="file">Background image</label>
           <input id="file" accept="image/*" name="file" type="file" @change="handleChangeFile" />
         </form>
-        <div class="space-x-2">
+        <div class="space-x-4">
           <TheButton @click="addPolygon()">Add polygon</TheButton>
           <TheButton @click="clearPoints()">Clear</TheButton>
         </div>
-        <div class="space-x-2">
+        <div class="space-x-4">
+          <label class="sr-only" for="step">Step</label>
+          <select
+            id="step"
+            v-model="step"
+            class="rounded-lg border bg-slate-900 px-2 py-1 text-slate-50 transition-colors hocus:bg-slate-800"
+            name="step"
+          >
+            <option v-for="option in steps" :key="option" :value="option">{{ option }}</option>
+          </select>
           <TheButton @click="zoomOut">-</TheButton>
           <TheButton @click="zoomIn">+</TheButton>
         </div>
       </div>
-      <div v-for="polygon in polygons" :key="polygon.id" class="flex items-center justify-between">
+      <div v-for="polygon in polygons" :key="polygon.id" class="flex items-center justify-between gap-4">
         <p>{{ polygon.id }} - {{ polygon.points.join(" ") }}</p>
         <div>
           <label class="sr-only" for="fill">Fill</label>
           <input id="fill" v-model="polygon.fill" name="fill" type="color" />
         </div>
-        <div class="flex items-center gap-2">
+        <div class="flex shrink-0 items-center gap-2">
           <TheButton @click="setActivePolygon(polygon.id)">Set active</TheButton>
           <TheButton @click="copyPoints(polygon.points)">{{ copied ? "Copied" : "Copy" }}</TheButton>
           <TheButton @click="deletePolygon(polygon.id)">Delete</TheButton>
